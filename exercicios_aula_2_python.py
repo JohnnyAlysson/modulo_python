b99f43a916a1f38ad6a9a9f5f15b3fbc02151067
JohnnyAlysson <johnalysson@hotmail.com>
2023-11-30 09:55:25 -0300

    alteração

--- exercicios_aula_2_python.py
+++ exercicios_aula_2_python.py
@@ -3,9 +3,11 @@
 
 lista = [10,False,85.6,"johnny"]
 
-print (type(lista))
-print(lista)
-print(lista[1])
-print(len(lista))
-lista[2] = 10.5
+print (type(lista)) # o programa vai apresentar list
+print(lista) 
+print(lista[1]) # programa imprime o 2° elemento da lista, pois o indice começa pelo 0
+print(len(lista)) # Len indica a quantidade de itens em uma lista
+lista[2] = 10.5   # Reatribuindo um novo item a uma posição especificifica na lista
+print(lista) 
+lista.append("oi") # append adiciona um item novo ao fim da lista
 print(lista)
