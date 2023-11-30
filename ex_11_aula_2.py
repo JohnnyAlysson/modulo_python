eb0f97c08c17078f822a99a2cee73a8f0f6d0584
JohnnyAlysson <johnalysson30@gmail.com>
2023-11-30 20:27:23 -0300

    mudança nomes

---
+++ ex_11_aula_2.py
@@ -0,0 +1,9 @@
+# 11. Crie uma lista com os nomes dos super-heróis que devem participar da Iniciativa Vingadores seguindo a ordem: 
+# • Homem de Ferro 
+# • Capitão América 
+# • Thor 
+# • Hulk 
+# • Viúva Negra 
+# • Gavião Arqueiro
+# a. Agora, inclua o Homem-Aranha no final da lista e imprima em qual posição está o Thor.
+# b. Infelizmente a Viúva Negra e o Homem de Ferro não fazem mais parte da Iniciativa Vingadores, então retire-os da lista.
