1d9fb100fdc2193742bcfd93f39159afaa412190
JohnnyAlysson <johnalysson30@gmail.com>
2023-12-02 00:01:55 -0300

    Resolução exercicio 11 Aula 2 do modulo de python

--- ex_11_aula_2.py
+++ ex_11_aula_2.py
@@ -7,3 +7,13 @@
 # • Gavião Arqueiro
 # a. Agora, inclua o Homem-Aranha no final da lista e imprima em qual posição está o Thor.
 # b. Infelizmente a Viúva Negra e o Homem de Ferro não fazem mais parte da Iniciativa Vingadores, então retire-os da lista.
+
+vingadores= []
+vingadores.extend(["Homem de Ferro","Capitão América","Thor","Hulk","Viúva Negra","Gavião Arqueiro"])
+print(vingadores)
+vingadores.append("Homem Aranha")
+print(vingadores)
+print(vingadores.index("Thor"))
+vingadores.remove("Viúva Negra")
+vingadores.remove("Homem de Ferro")
+print(vingadores)
