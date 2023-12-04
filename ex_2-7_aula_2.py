037f6596774c08f86b6fbc83113511a115298b5c
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2023-12-04 07:56:45 -0300

    Atualizar o ex_2-7_aula_2.py

--- ex_2-7_aula_2.py
+++ ex_2-7_aula_2.py
@@ -1,1 +1,17 @@
+# 2. Ler uma lista de 5 números inteiros #e mostre cada número da lista. 
+#3. Ler uma lista de 10 números inteiros #e mostre-os na ordem inversa. 
+#4. Ler uma lista com 4 notas, em seguida #o programa deve exibir as notas e a #média. 
+#5. Ler uma lista com 20 idades e exibir #a maior e menor.
+#6. Faça um programa que armazene 10 #letras em uma lista e imprima uma #listagem numerada.
+#7. Ler uma lista de 15 números inteiros #e mostre cada número juntamente com a #sua posição na lista. 
+
+
+
+
+
+
+
+
+
+
 
