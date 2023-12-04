f26d4a20902aff57758d64e129ddbbaf9bbd484e
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2023-12-04 08:02:14 -0300

    Atualizar o ex_2-7_aula_2.py

--- ex_2-7_aula_2.py
+++ ex_2-7_aula_2.py
@@ -1,4 +1,12 @@
 # 2. Ler uma lista de 5 números inteiros #e mostre cada número da lista. 
+lista = []
+
+for i in range(5):
+numero= input(f"digite o {i}° número:")
+
+
+
+
 #3. Ler uma lista de 10 números inteiros #e mostre-os na ordem inversa. 
 #4. Ler uma lista com 4 notas, em seguida #o programa deve exibir as notas e a #média. 
 #5. Ler uma lista com 20 idades e exibir #a maior e menor.
