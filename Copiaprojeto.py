9ea781108cc149b3eb73d1c4e00cf5a975f5e820
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2023-12-07 09:39:51 -0300

    mudanças

---
+++ Copiaprojeto.py
@@ -0,0 +1,11 @@
+# importar módulo
+import pandas as pd
+
+#importar base de dados
+tabela=pd.read("Vendas.xlsx")
+
+# visualizar base de dados
+# faturamento por loja
+# quantidade de produtos vendidos por loja
+# ticket medio por produto em cada loja
+# enviar um email com relatorio
