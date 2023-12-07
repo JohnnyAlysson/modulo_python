5796f02086bdb13a4e15fa323331c7022c98a784
JohnnyAlysson <johnalysson30@gmail.com>
2023-12-07 10:06:01 -0300

    salvar

--- Copiaprojeto.py
+++ Copiaprojeto.py
@@ -1,10 +1,13 @@
 # importar módulo
-import pandas as pd
+import pandas as pd #importando p módulo pandas e nomeamos ele "pd" para trabalhar com arquivos excel
 
 #importar base de dados
-tabela=pd.read("Vendas.xlsx")
+tabela=pd.read_excel("Vendas.xlsx") #definimos a variavel tabela, que é a leitura da tabela no excel
 
 # visualizar base de dados
+pd.set_option() #
+print(tabela)
+
 # faturamento por loja
 # quantidade de produtos vendidos por loja
 # ticket medio por produto em cada loja
