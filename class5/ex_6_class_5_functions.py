e6f01a22c1d0741f2631b16779949fed52484a5c
JohnnyAlysson <johnalysson30@gmail.com>
2023-12-21 15:24:16 -0300

    o

--- class5/ex_6_class_5_functions.py
+++ class5/ex_6_class_5_functions.py
@@ -1,2 +1,19 @@
 # 6. Faça um programa que verifique se um número é primo por meio de um função.
 # Ao final imprima o resultado.
+
+
+
+def checkNumberPrime(number):
+    for i in range(2,number):
+        if number % i == 0 or number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0 or number % 11 == 0:
+            print(f" Número {number} não é primo")
+            break
+        else:
+            print(f" Número {number} é primo")
+            break
+    
+    
+
+number= int(input("Digite um número:"))
+
+checkNumberPrime(number)
---
+++ class5/ex_7_class_5_functions.py
@@ -0,0 +1,6 @@
+# 7. Faça um programa que leia a base e a altura de um retângulo e imprima o perí-
+# metro, a área e a diagonal. Para fazer os cálculos, implemente três funções, cada
+# uma deve realizar um cálculo especifico conforme solicitado. Utilize as fórmulas
+# a seguir.
+
+def calc(l1,l2)
