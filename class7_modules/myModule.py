b2b44c421e666fa47ebcaf13a4a3b56006af0e39
JohnnyAlysson <johnalysson30@gmail.com>
2024-01-18 15:17:18 -0300

    exercise 9 solved

--- class7_modules/myModule.py
+++ class7_modules/myModule.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 def changeMonth(date):
     breaking_date = []
@@ -15,3 +16,27 @@ def changeMonth(date):
     new_date=datetime.date(year,month,day)
     
     return new_date.strftime("%d %B, %Y")  
+
+
+def rollDie():
+    count1 = count2=count3=count4=count5=count6 = 0
+    for i in range (1000000):
+        die_roll=random.randint(1,6)
+        if die_roll == 1:
+            count1 +=1
+        elif die_roll ==2:
+            count2 += 1
+        elif die_roll ==3:
+            count3 += 1
+        elif die_roll ==4:
+            count4 += 1
+        elif die_roll ==5:
+            count5 += 1
+        elif die_roll ==6:
+            count6 += 1
+    print(f" number 1 appeared {round((count1/1000000),2)*100} %")
+    print(f" number 2 appeared {round((count2/1000000),2)*100} %")
+    print(f" number 3 appeared {round((count3/1000000),2)*100} %")
+    print(f" number 4 appeared {round((count4/1000000),2)*100} %")
+    print(f" number 5 appeared {round((count5/1000000),2)*100} %")
+    print(f" number 6 appeared {round((count6/1000000),2)*100} %")
