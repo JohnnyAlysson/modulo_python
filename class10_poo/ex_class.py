5583ca501f3893feadc8b4707e4ec97b325e25d8
JohnnyAlysson <johnalysson30@gmail.com>
2024-02-15 10:53:50 -0300

    Add ex class

---
+++ class10_poo/ex_class.py
@@ -0,0 +1,20 @@
+class Carro():
+	#atributos
+	#cor,modelo,ano,marca
+	def __init__(self,cor,modelo,ano,marca): #Construtor
+		self.cor = cor
+		self.modelo = modelo
+		self.ano = ano
+		self.marca = marca
+	#métodos
+	def ligar(self):
+		print(f"O {self.modelo} está ligado !!")
+
+
+
+###### fora da classe ###########
+
+carro1 = Carro("prata","hilux","2020","Toyota")
+carro2 = Carro("vermelho","moby","2016","Fiat")
+print(carro1.marca)
+print(carro2.ligar())
