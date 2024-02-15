9cee81259a704044ff4aa4c0aae64b59571eb33b
JohnnyAlysson <johnalysson30@gmail.com>
2024-02-15 12:26:21 -0300

    create new files

---
+++ class10_poo/ex_3_class_10_modules.py
@@ -0,0 +1,8 @@
+# Crie uma classe Empresa que permita gerenciar
+# funcionários. Os funcionários devem ter informações
+# como nome, cargo e salário. A empresa deve ser capaz
+# de adicionar, remover e listar funcionários.
+
+class Empresa:
+    def __init__(self,funcionario):
+        pass
---
+++ class11_poo_II/ex_class.py
@@ -0,0 +1,10 @@
+class Conta():
+    def __init__(self,nome,saldo,n_da_conta):
+        self.nome = nome
+        self.saldo = saldo
+        self.n_da_conta = n_da_conta
+        self.lista = []
+    
+	def deposito():
+        
+        
