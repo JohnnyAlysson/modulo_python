9136e0f64f590ad275fa4ad8310a0386d86c539d
JohnnyAlysson <johnalysson30@gmail.com>
2024-02-15 11:41:30 -0300

    Solve exercise 1

---
+++ class10_poo/ex_1_class_10_modules.py
@@ -0,0 +1,18 @@
+# Crie um classe chamada cachorro com os atributos:
+# nome, raça, idade
+
+class Cachoro():
+
+    def __init__(self,nome,raça,idade) :
+        self.nome = nome
+        self.raça = raça
+        self.idade = idade
+
+    def latir(self):
+        print(f" {self.nome} : Au Au")
+
+
+cachorro1 = Cachoro("Fuxico","SRD","5")
+cachorro2 = Cachoro("Lulu","Akita","3")
+
+cachorro1.latir()
